52391eac7f8715caee0f1b050eb7aebd8a02d8d4
iqiraw98-spec <iqiraw98@gmail.com>
2026-06-07 10:26:56 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+// صيدلية النسور - Service Worker v2.0
+const CACHE_NAME = 'nasoor-v2';
+const ASSETS = [
+  '/Iqi22/',
+  '/Iqi22/index.html',
+  '/Iqi22/manifest.json',
+  '/Iqi22/icon-192.png',
+  '/Iqi22/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(res => {
+        if (res && res.status === 200 && res.type === 'basic') {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || network;
+    })
+  );
+});
